ffdf02ea46059ba748cb991843746eb9026e0cc2
Dev <dev@0v1se.com>
2018-03-05 00:02:50 +0300

    PublicSale deployed to prod

--- source/config/common.js
+++ source/config/common.js
@@ -2,7 +2,7 @@
 
 const sales = {
   development: '0xaee0eb707a23fee852c43b5eb22030a25a729937',
-  production: '0x99a09f0d85bc6e95e110348a8522f98443e31c4a',
+  production: '0xf76350f0ad6b7cfce1311ef43e2eef488fd16dad',
 };
 
 export const realm = '5a8d7ce7a302eb3601fdef2c';
